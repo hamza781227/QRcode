8bb968915e747fb60f73e3582204aeb786a4263b
hamza badr <163992184+hamza781227@users.noreply.github.com>
2024-03-25 12:04:43 +0100

    Add files via upload

---
+++ Notice_Numérique.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from streamlit_pdf_viewer import pdf_viewer
+
+
+st.set_page_config(
+    page_title='Homepage',
+    page_icon='📃',
+    layout='wide',
+    initial_sidebar_state='auto'
+)
+
+st.title('📃 Notice Numérique Doliprane 💊')
+
+st.divider()
+
+
+tab1,tab2,tab3 = st.tabs(['Fiche info','Résumé des caractéristiques du produit','Notice'])
+
+with tab1:
+    pdf_viewer("Fiche-info-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
+
+with tab2:
+     pdf_viewer("Résumé-des-caractéristiques-du-produit-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
+
+with tab3:
+     pdf_viewer("Notice-patient-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
