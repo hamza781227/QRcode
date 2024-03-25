424aea2148dde5a5a80c083f20b4312ede2ca4d7
hamza badr <163992184+hamza781227@users.noreply.github.com>
2024-03-25 12:11:56 +0100

    Add files via upload

---
+++ pages/Vidéo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title='Homepage',
+    page_icon='🎥',
+    layout='wide',
+    initial_sidebar_state='auto'
+)
+
+st.title('Vidéo Notice')
+
+st.video('https://www.youtube.com/watch?v=pCnPalYdC80')
+
