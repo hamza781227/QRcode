54a9bd31262916a916e325dbd0735e7e3f0b6dd7
hamza badr <163992184+hamza781227@users.noreply.github.com>
2024-03-25 13:52:09 +0100

    Update Notice_Numérique.py

--- Notice_Numérique.py
+++ Notice_Numérique.py
@@ -18,10 +18,10 @@ st.divider()
 tab1,tab2,tab3 = st.tabs(['Fiche info','Résumé des caractéristiques du produit','Notice'])
 
 with tab1:
-    pdf_viewer("Fiche-info-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
+    pdf_viewer("Fiche-info-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf")
 
 with tab2:
-     pdf_viewer("Résumé-des-caractéristiques-du-produit-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
+     pdf_viewer("Résumé-des-caractéristiques-du-produit-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf")
 
 with tab3:
-     pdf_viewer("Notice-patient-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf",width=1200)
+     pdf_viewer("Notice-patient-DOLIPRANE-1000-mg-comprimé-Base-de-données-publique-des-médicaments.pdf")
